3aab9fd22d626731a08b5090ee6f7c28871f6b66
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2026-06-25 17:00:47 +0600

    feat: jwt access & refresh token added to login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,17 +5,39 @@ import { catchAsync } from "../../utils/catchAsync";
 import { authService } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
 import type { ILoginPayload } from "./auth.interface";
+import { jwtUtils } from "../../utils/jwt";
+import config from "../../config";
+import type { SignOptions } from "jsonwebtoken";
 
 const loginUser = catchAsync(async (req: Req, res: Res, next: NextFunction) => {
   const payload = req.body as ILoginPayload;
 
   const loginResult = await authService.loginUser(payload);
 
+  const jwtPayload = {
+    id: loginResult.id,
+    email: loginResult.email,
+    role: loginResult.role,
+    name: loginResult.name,
+  };
+
+  const accessToken = jwtUtils.createJWTToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+
+  const refreshToken = jwtUtils.createJWTToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
+
   sendResponse(res, {
     succces: true,
     statusCode: httpStatus.OK,
     message: "login successfull",
-    data: loginResult,
+    data: { accessToken, refreshToken },
   });
 });
 
